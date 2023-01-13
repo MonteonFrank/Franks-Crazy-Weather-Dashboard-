6663e33633c652a1595b1a3297967273e2a824a2
Frank Monteon <frank.monteon@gmail.com>
2023-01-13 13:03:25 -0600

    Added the API calls and now getting response from the 3rd party server

---
+++ Assets/JavaScript/script.js
@@ -0,0 +1,45 @@
+var requestURL = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=d21b12879c795a0cfa44c139988b84a4';
+var latitude = "";
+var longitude = "";
+var cityname = "";
+ 
+$(document).ready(function () {
+function getlocation(){
+fetch (requestURL)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        for(var i =0; i<data.length ; i++){
+
+            latitude = data[i].lat
+            longitude = data[i].lon
+
+            console.log((data[i].name));
+            console.log(data[i].lat);
+            console.log(data[i].lon);
+
+            var weather = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=metric&appid=d21b12879c795a0cfa44c139988b84a4";
+           
+            console.log(weather);
+
+                fetch(weather)
+                    .then(function (response2) {
+                          return response2.json();
+                    })
+                            .then(function (data2) {
+                        
+                                 console.log(data2.main)        
+                             });
+            }
+    })
+}
+
+
+
+$('#button1').on("click", function(){
+    alert("button was clicked");
+    getlocation();
+})
+
+});
